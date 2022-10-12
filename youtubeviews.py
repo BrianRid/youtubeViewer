e6063b818fa85e794d060c3624a4820cdf09b125
BrianRid <brianridolce@gmail.com>
2022-10-12 10:16:14 +0200

    feat: adjusting XPath to autoclick refuse cookies with the new youtube interface

--- youtubeviews.py
+++ youtubeviews.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from pyvirtualdisplay import Display
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 import time
 import os
 
@@ -11,14 +14,19 @@ views = int(input('Enter the number of views you want to add : '))
 
 
 for i in range(views):
-  chromedriver = './chromedriver'
+  chromedriver = '../chromedriver'
   os.environ['webdriver.chrome.driver'] = chromedriver
   driver = webdriver.Chrome(chromedriver)
   driver.get(url)
   time.sleep(25)
-  no_button = driver.find_element(By.XPATH, "//span[contains(text(), 'Tout refuser')]")
+  no_button = driver.find_element(By.XPATH, "//tp-yt-paper-button[@id='button']/yt-formatted-string[contains(text(),'Tout refuser')]")
+  if no_button:
+    ActionChains(driver).move_to_element(no_button).click(no_button).perform()
+    time.sleep(120)
+    driver.close()
+  else:
+    print('No button found')
+    time.sleep(120)
+    driver.close()
 
-  ActionChains(driver).move_to_element(no_button).click(no_button).perform()
 
-  time.sleep(120)
-  driver.close()
